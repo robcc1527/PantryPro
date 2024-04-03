8f18da761ea6f9b75643627f8271c8f1eef546c4
Dennis Garcia <BlueBoxDev@outlook.com>
2024-04-02 19:08:37 -0500

    had to many divs

--- pantrypro.client/src/App.tsx
+++ pantrypro.client/src/App.tsx
@@ -1,73 +1,81 @@
-import { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
 //Button
-import Button from './Components/Buttons/Button';
+import Button from "./Components/Buttons/Button";
 
 interface Forecast {
-    date: string;
-    temperatureC: number;
-    temperatureF: number;
-    summary: string;
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
 }
 
 function App() {
-    const [forecasts, setForecasts] = useState<Forecast[]>();
+  const [forecasts, setForecasts] = useState<Forecast[]>();
 
-    useEffect(() => {
-        populateWeatherData();
-    }, []);
+  useEffect(() => {
+    populateWeatherData();
+  }, []);
 
-    const contents = forecasts === undefined
-        ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-        : <table className="table table-striped" aria-labelledby="tabelLabel">
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Temp. (C)</th>
-                    <th>Temp. (F)</th>
-                    <th>Summary</th>
-                </tr>
-            </thead>
-            <tbody>
-                {forecasts.map(forecast =>
-                    <tr key={forecast.date}>
-                        <td>{forecast.date}</td>
-                        <td>{forecast.temperatureC}</td>
-                        <td>{forecast.temperatureF}</td>
-                        <td>{forecast.summary}</td>
-                    </tr>
-                )}
-            </tbody>
-        </table>;
-
-    return (
-        <Router>
-            <Routes>
-                {/* <Route path='/' element={<Dashboard />}/> */}
-                {/* <Route path='/pantry' element={<Pantry />}/> */}
-            </Routes>
-            <div>
-                <h1 id="tabelLabel">Weather forecast</h1>
-                <p>This component demonstrates fetching data from the server.</p>
-                {contents}
-                <div>
-                    My name is Chezy. Hello World!
-                    My name is Izzy. Hello World!!! 
-                    My name is Esteban. Hello World!
-                    Test
-                </div>
-            </div>
-            <Button/>
-        </div>
-        </Router>
+  const contents =
+    forecasts === undefined ? (
+      <p>
+        <em>
+          Loading... Please refresh once the ASP.NET backend has started. See{" "}
+          <a href="https://aka.ms/jspsintegrationreact">
+            https://aka.ms/jspsintegrationreact
+          </a>{" "}
+          for more details.
+        </em>
+      </p>
+    ) : (
+      <table className="table table-striped" aria-labelledby="tabelLabel">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Temp. (C)</th>
+            <th>Temp. (F)</th>
+            <th>Summary</th>
+          </tr>
+        </thead>
+        <tbody>
+          {forecasts.map((forecast) => (
+            <tr key={forecast.date}>
+              <td>{forecast.date}</td>
+              <td>{forecast.temperatureC}</td>
+              <td>{forecast.temperatureF}</td>
+              <td>{forecast.summary}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     );
 
-    async function populateWeatherData() {
-        const response = await fetch('weatherforecast');
-        const data = await response.json();
-        setForecasts(data);
-    }
+  return (
+    <Router>
+      <Routes>
+        {/* <Route path='/' element={<Dashboard />}/> */}
+        {/* <Route path='/pantry' element={<Pantry />}/> */}
+      </Routes>
+      <div>
+        <h1 id="tabelLabel">Weather forecast</h1>
+        <p>This component demonstrates fetching data from the server.</p>
+        {contents}
+        <div>
+          My name is Chezy. Hello World! My name is Izzy. Hello World!!! My name
+          is Esteban. Hello World! Test
+        </div>
+      </div>
+      <Button />
+    </Router>
+  );
+
+  async function populateWeatherData() {
+    const response = await fetch("weatherforecast");
+    const data = await response.json();
+    setForecasts(data);
+  }
 }
 
 export default App;
