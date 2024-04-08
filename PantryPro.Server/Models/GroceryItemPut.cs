0a21fee566a288682d681468844da068a970aa27
Robert <robcc1527@gmail.com>
2024-04-08 14:28:37 -0500

    Rest API PUT working for all parameters

---
+++ PantryPro.Server/Models/GroceryItemPut.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace PantryPro.Server
+{
+    public class GroceryItemPut
+    {
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Description { get; set; } = default!;
+
+        public int Protein { get; set; }
+
+        public int GroceryItemTypeId { get; set; }
+
+        public int Carbs { get; set; }
+
+        public int Calories { get; set; }
+
+        public int Weight { get; set; }
+
+        public int Fat { get; set; }
+    }
+}
