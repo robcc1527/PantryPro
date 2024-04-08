b280893441e48d1bc2a0ae132f1ebe50a5585a2c
esteban-almendariz <98506796+esteban-almendariz@users.noreply.github.com>
2024-04-07 23:18:29 -0500

    add function to filter items in pantry list

--- pantrypro.client/src/Pages/Dashboard.tsx
+++ pantrypro.client/src/Pages/Dashboard.tsx
@@ -1,8 +1,32 @@
 
-const Dashboard = () => {
+const Dashboard = ({pantryList}) => {
+
+    const displayItems = pantryList.map(item => (
+        <div key={item.id} className="item-dashboard">
+            <span>{item.item}</span>
+            <span>{item.description}</span>
+            <span>{item.quantity}</span>
+        </div>
+    ))
+
+    console.log('pantr list: ', pantryList)
+
     return (
         <div>
-            <h1>Dashboard Page</h1>
+            <h1 className="title">Dashboard Page</h1>
+            <div>
+                <h2>Pantry List</h2>
+                <div className="title-list-dashboard">
+                    <h3>Item</h3>
+                    <h3>Item Type</h3>
+                    <h3>Quantity</h3>
+                </div>
+            </div>
+            <div className="item-dashboard-container">
+                
+                {pantryList.length == 0 ? 'Add item from pantry list' : displayItems}
+                
+            </div>
         </div>
     )
 }
