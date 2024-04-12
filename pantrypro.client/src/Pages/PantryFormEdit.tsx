4a44050ab18e95aac995ab59a2384c69398acd55
Dennis Garcia <BlueBoxDev@outlook.com>
2024-04-11 21:45:48 -0500

    update pantry form

--- pantrypro.client/src/Pages/PantryFormEdit.tsx
+++ pantrypro.client/src/Pages/PantryFormEdit.tsx
@@ -1,47 +1,66 @@
-import React, { useState } from 'react';
-import './PantryForm.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./PantryForm.css";
 
 const PantryForm: React.FC = () => {
   // State to store form data
+  const [isLoading, setIsLoading] = useState(true);
+  const [groceryItemTypes, setGroceryItemTypes] = useState([] as any[]);
   const [formData, setFormData] = useState({
-    name: '',
-    description: '',
+    name: "",
+    description: "",
     weight: 0,
-    groceryItemTypeId: 0,
+    groceryItemTypeId: -1,
     carbs: 0,
     calories: 0,
     protein: 0,
-    fat: 0
+    fat: 0,
   });
 
-  // Handle form input changes
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-  const { name, value } = e.target;
-  setFormData({ ...formData, [name]: value });
-};
+  useEffect(() => {
+    // Fetch data from the server using axios
+    axios.get("http://localhost:5206/GroceryItemType").then((response) => {
+      setIsLoading(false);
+      setGroceryItemTypes(response.data);
+    });
+  }, []);
 
+  // Handle form input changes
+  const handleInputChange = (
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >
+  ) => {
+    const { name, value } = e.target;
+    const tempFormData = { ...formData, [name]: value };
+    setFormData(tempFormData);
+  };
 
   // Handle form submission
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     // Perform any necessary actions, such as sending data to a server
-    console.log('Form submitted:', formData);
-    // Reset form fields
+    console.log("Form submitted:", formData);
+    //Reset form fields
     setFormData({
-    name: '',
-    description: '',
-    weight: 0,
-    groceryItemTypeId: 0,
-    carbs: 0,
-    calories: 0,
-    protein: 0,
-    fat: 0
+      name: "",
+      description: "",
+      weight: 0,
+      groceryItemTypeId: 0,
+      carbs: 0,
+      calories: 0,
+      protein: 0,
+      fat: 0,
     });
   };
 
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
-      <h1>Pantry Form</h1>
+      <h1>Pantry Edit Item</h1>
       <form onSubmit={handleSubmit}>
         <label>
           Name:
@@ -75,13 +94,17 @@ const PantryForm: React.FC = () => {
         </label>
         <br />
         <label>
-          Grocery Item Type ID:
-          <input
-            type="number"
-            name="groceryItemTypeId"
-            value={formData.groceryItemTypeId}
-            onChange={handleInputChange}
-          />
+          Grocery Item Type:
+          <select name="groceryItemTypeId" onChange={handleInputChange}>
+            <option value={-1}>Select grocery item type...</option>
+            {groceryItemTypes.map((item) => {
+              return (
+                <option key={item.id} value={item.id}>
+                  {item.description}
+                </option>
+              );
+            })}
+          </select>
         </label>
         <br />
         <label>
